8a4be377c2f8a59c76446e5a039d1a051f1e3082
mukashov1 <bakdaulet.mukashov@gmail.com>
2021-11-06 22:19:02 +0600

    php

---
+++ sender.php
@@ -0,0 +1,23 @@
+<?php
+    $name = $_POST['name'];
+    // $surname = $_POST['surname'];
+	// $phone = $_POST['phone'];
+    // $email = $_POST['email'];
+    // $text = $_POST['text'];
+
+	$to = "bakdaulet.mukashov@gmail.com"; 
+	$date = date ("d.m.Y"); 
+	$time = date ("h:i");
+	$from = $email;
+	$subject = "Заявка c сайта";
+
+	
+	$msg="
+    Имя: $name /n
+    // Фамилия: $surname /n
+    // Телефон: $phone /n
+    // Почта: $email /n
+    // Текст: $text"; 	
+	mail($to, $subject, $msg, "From: $from ");
+
+?>
